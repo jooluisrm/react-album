f9cc42c12a33307b9a7c06c95c27e876a30fc683
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-04 15:45:30 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+    return (
+        <div>
+            <header className="py-5 border-b-2">
+                <h1 className="text-4xl font-bold">Galeria de Fotos</h1>
+            </header>
+            <div>
+
+            </div>
+        </div>
+    );
+}
+
+export default App;
